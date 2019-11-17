6d0c8a940c37e30845cdc24f5c312648050d2a6b
attila5287 <atiturkoz@hotmail.com>
2019-11-17 02:46:18 -0700

    fx import os

--- jampayroll/__init__.py
+++ jampayroll/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
